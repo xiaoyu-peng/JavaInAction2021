e77a2d32607ed8402c1e234f5d70f30e4ae11fe7
PengHui <penghui@udesk.cn>
2021-06-30 22:30:59 +0800

    XH-4 原型设计模式

---
+++ src/main/java/com/xhh/DesignPatterns/Creation_Mode_6/D005_PrototypePatterns/FAR.java
@@ -0,0 +1,28 @@
+package com.xhh.DesignPatterns.Creation_Mode_6.D005_PrototypePatterns;
+
+
+/**
+ * //可行性分析报告(Feasibility Analysis Report)类
+ */
+public class FAR implements OfficialDocument {
+
+    @Override
+    public OfficialDocument clone() {
+
+        OfficialDocument far = null;
+        try {
+            far = (OfficialDocument) super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("不支持复制！");
+        }
+
+        return far;
+    }
+
+
+    @Override
+    public void display() {
+        System.out.println("《可行性分析报告》");
+    }
+
+}
---
+++ src/main/java/com/xhh/DesignPatterns/Creation_Mode_6/D005_PrototypePatterns/OfficialDocument.java
@@ -0,0 +1,12 @@
+package com.xhh.DesignPatterns.Creation_Mode_6.D005_PrototypePatterns;
+
+/**
+ * 抽象公文接口，也可定义为抽象类，提供clone()方法的实现，将业务方法声明为抽象方法
+ */
+interface OfficialDocument extends Cloneable {
+
+    public OfficialDocument clone();
+
+    public void display();
+
+}
