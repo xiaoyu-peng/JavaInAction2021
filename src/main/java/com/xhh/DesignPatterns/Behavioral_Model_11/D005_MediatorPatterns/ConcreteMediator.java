5a5c1c934c425e65f1db6952e0713ac9182e00b3
PengHui <penghui@udesk.cn>
2021-07-05 23:16:55 +0800

    XH-18 中介者模式

---
+++ src/main/java/com/xhh/DesignPatterns/Behavioral_Model_11/D005_MediatorPatterns/ConcreteMediator.java
@@ -0,0 +1,43 @@
+package com.xhh.DesignPatterns.Behavioral_Model_11.D005_MediatorPatterns;
+
+
+/**
+ * 具体中介者
+ */
+class ConcreteMediator extends Mediator {
+
+	/**
+	 * 维持对各个同事对象的引用
+	 */
+	public Button addButton;
+    public List list;
+    public TextBox userNameTextBox;
+    public ComboBox cb;
+
+	/**
+	 * 封装同事对象之间的交互
+	 * @param c
+	 */
+	@Override
+	public void componentChanged(Component c) {
+        //单击按钮
+        if (c == addButton) {
+            System.out.println("--单击增加按钮--");
+            list.updateOP();
+            cb.updateOP();
+            userNameTextBox.updateOP();
+        }
+        //从列表框选择客户
+        else if (c == list) {
+            System.out.println("--从列表框选择客户--");
+            cb.selectOP();
+            userNameTextBox.setText();
+        }
+        //从组合框选择客户
+        else if (c == cb) {
+            System.out.println("--从组合框选择客户--");
+            cb.selectOP();
+            userNameTextBox.setText();
+        }
+    }
+}
---
+++ src/main/java/com/xhh/DesignPatterns/Behavioral_Model_11/D005_MediatorPatterns/Mediator.java
@@ -0,0 +1,8 @@
+package com.xhh.DesignPatterns.Behavioral_Model_11.D005_MediatorPatterns;
+
+/**
+ * 抽象中介者
+ */
+abstract class Mediator {
+    public abstract void componentChanged(Component c);
+}
