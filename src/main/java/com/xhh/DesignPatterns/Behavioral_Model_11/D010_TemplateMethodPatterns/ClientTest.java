4218a01dc958113a426cb803438553ec192f56f6
PengHui <penghui@udesk.cn>
2021-07-06 23:06:34 +0800

    XH-22 模板方法模式

---
+++ src/main/java/com/xhh/DesignPatterns/Behavioral_Model_11/D010_TemplateMethodPatterns/ClientTest.java
@@ -0,0 +1,19 @@
+package com.xhh.DesignPatterns.Behavioral_Model_11.D010_TemplateMethodPatterns;
+
+/**
+ * 某软件公司欲为某银行的业务支撑系统开发一个利息计算模块，利息计算流程如下：
+ *
+ *       (1) 系统根据账号和密码验证用户信息，如果用户信息错误，系统显示出错提示；
+ *
+ *       (2) 如果用户信息正确，则根据用户类型的不同使用不同的利息计算公式计算利息（如活期账户和定期账户具有不同的利息计算公式）；
+ *
+ *       (3) 系统显示利息。
+ *       
+ */
+public class ClientTest {
+
+    public static void main(String[] args) {
+        AbsAccount account = new SavingAccount();
+        account.Handle("张三", "123456");
+    }
+}
